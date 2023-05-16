efd9e8ab05f8f6aa52ac0d64eb93720a445ce6ad
x0y14 <ya01ma08@gmail.com>
2023-05-16 16:11:14 +0900

    戻り値を持つリターンのみの関数を解析できるようにいくつかを実装

--- typecheck/typecheck.go
+++ typecheck/typecheck.go
@@ -1,8 +1,15 @@
 package typecheck
 
+import (
+	"fmt"
+	"github.com/x0y14/beat/core"
+	"github.com/x0y14/beat/parse"
+)
+
 type TypeChecker struct {
 	curtNest int       // 深さ
 	curtTree *TypeTree // 現在参照してるやつ
+	curtFn   *Function
 	Tree     *TypeTree // 全体
 }
 
@@ -10,8 +17,9 @@ func NewTypeChecker() *TypeChecker {
 	t := NewTypeTree()
 	return &TypeChecker{
 		curtNest: 0,
-		Tree:     t,
 		curtTree: t,
+		curtFn:   nil,
+		Tree:     t,
 	}
 }
 
@@ -42,6 +50,7 @@ func (tc *TypeChecker) SetFunction(name string, f *Function, focus bool) {
 	// 注目する場合は差し替える
 	if focus {
 		tc.curtTree = f.TypeTree
+		tc.curtFn = f
 		tc.curtNest++
 	}
 }
@@ -56,6 +65,7 @@ func (tc *TypeChecker) FindFunctionInCurrent(name string, focus bool) (*Function
 		if fName == name {
 			if focus {
 				tc.curtTree = f.TypeTree
+				tc.curtFn = f
 			}
 			return f, true
 		}
@@ -76,6 +86,7 @@ func (tc *TypeChecker) FindFunctionConsiderNest(name string, focus bool) (*Funct
 		if ok {
 			tc.curtNest = n // 特にここバグになりやすそう
 			tc.curtTree = f.TypeTree
+			tc.curtFn = f
 			return f, ok
 		}
 	}
@@ -112,4 +123,187 @@ func (tc *TypeChecker) FindVariableConsiderNest(name string) (*Variable, bool) {
 	// 復元
 	tc.curtTree = tmp
 	return nil, false
+}
+
+var tc *TypeChecker
+
+func TypeCheck(nodes []*parse.Node) (*TypeTree, error) {
+	tc = NewTypeChecker()
+	for _, node := range nodes {
+		_, err := toplevel(node)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return tc.Tree, nil
+}
+
+func toplevel(node *parse.Node) ([]core.Type, error) {
+	switch node.Kind {
+	case parse.FuncDef:
+		return fn(node)
+	case parse.VarDecl:
+	default:
+		return stmt(node)
+	}
+	return nil, fmt.Errorf("toplevel unimplemented")
+}
+
+func fn(node *parse.Node) ([]core.Type, error) {
+	field := node.FuncDefField
+	name := field.Identifier.IdentField.Ident
+
+	// 引数
+	var parameters []*Variable
+	if field.Parameters != nil {
+		for _, paramNode := range field.Parameters.PolynomialField.Values {
+			param := paramNode.FuncParam
+			parameters = append(parameters, NewVariable(
+				param.Identifier.IdentField.Ident,
+				param.DataType.DataTypeField.Type))
+		}
+	}
+
+	// 戻り値
+	var returns []*Variable
+	if field.Returns != nil {
+		for _, rtNode := range field.Returns.PolynomialField.Values {
+			returns = append(returns, NewVariable("", rtNode.DataTypeField.Type))
+		}
+	}
+
+	// グローバルにくっつけてあげる
+	f := NewFunction(parameters, returns)
+	tc.SetFunction(name, f, true)
+
+	// 関数の中身を解析
+	// 戻り値はReturnsにあるものを使うので捨てる
+	_, err := stmt(field.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	// グローバルに戻す
+	tc.GoParent()
+	return nil, nil
+}
+
+func stmt(node *parse.Node) ([]core.Type, error) {
+	switch node.Kind {
+	case parse.Return:
+		var rtTypes []core.Type
+		for _, val := range node.PolynomialField.Values {
+			t, err := expr(val)
+			if err != nil {
+				return nil, err
+			}
+			rtTypes = append(rtTypes, t...)
+		}
+		if !isSameType(variablesToTypes(tc.curtFn.Returns), rtTypes) {
+			return nil, fmt.Errorf("戻り値の型が異なる: %v, %v", variablesToTypes(tc.curtFn.Returns), rtTypes)
+		}
+		return rtTypes, nil
+	case parse.IfElse:
+	case parse.For:
+	case parse.Block:
+		for _, statement := range node.BlockField.Statements {
+			_, err := stmt(statement)
+			if err != nil {
+				return nil, err
+			}
+		}
+		return nil, nil
+	default:
+		return expr(node)
+	}
+	return nil, fmt.Errorf("stmt unimplemented")
+}
+
+func expr(node *parse.Node) ([]core.Type, error) {
+	return assign(node)
+}
+
+func assign(node *parse.Node) ([]core.Type, error) {
+	switch node.Kind {
+	case parse.VarDecl:
+	case parse.ShortVarDecl:
+	case parse.Assign:
+	default:
+		return andor(node)
+	}
+	return nil, fmt.Errorf("assign unimplemented")
+}
+
+func andor(node *parse.Node) ([]core.Type, error) {
+	switch node.Kind {
+	case parse.And, parse.Or:
+	default:
+		return equality(node)
+	}
+	return nil, fmt.Errorf("andor unimplemented")
+}
+
+func equality(node *parse.Node) ([]core.Type, error) {
+	switch node.Kind {
+	case parse.Eq, parse.Ne:
+	default:
+		return relational(node)
+	}
+	return nil, fmt.Errorf("equality unimplemnted")
+}
+
+func relational(node *parse.Node) ([]core.Type, error) {
+	switch node.Kind {
+	case parse.Lt, parse.Le, parse.Gt, parse.Ge:
+	default:
+		return add(node)
+	}
+	return nil, fmt.Errorf("relational unimplemented")
+}
+
+func add(node *parse.Node) ([]core.Type, error) {
+	switch node.Kind {
+	case parse.Add:
+	case parse.Sub:
+	default:
+		return mul(node)
+	}
+	return nil, fmt.Errorf("add unimplemented")
+}
+
+func mul(node *parse.Node) ([]core.Type, error) {
+	switch node.Kind {
+	case parse.Mul, parse.Div, parse.Mod:
+	default:
+		return unary(node)
+	}
+	return nil, fmt.Errorf("mul unimplemented")
+}
+
+func unary(node *parse.Node) ([]core.Type, error) {
+	switch node.Kind {
+	case parse.Not:
+	default:
+		return primary(node)
+	}
+	return nil, fmt.Errorf("unary unimplemented")
+}
+
+func primary(node *parse.Node) ([]core.Type, error) {
+	return access(node)
+}
+
+func access(node *parse.Node) ([]core.Type, error) {
+	return literal(node)
+}
+
+func literal(node *parse.Node) ([]core.Type, error) {
+	switch node.Kind {
+	case parse.Parenthesis:
+	case parse.Ident:
+	case parse.Call:
+	default:
+		return wrap(node.LiteralField.Literal.GetKind()), nil
+	}
+	return nil, fmt.Errorf("literal unimplemented")
 }
